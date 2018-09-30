e370d2a1d32ce51edf3e5a653470af79661c6c3c
Brad Baker <bbaker@atlassian.com>
2018-09-30 22:08:10 +1000

    More JavaDoc

--- src/main/java/graphql/scalars/ExtendedScalars.java
+++ src/main/java/graphql/scalars/ExtendedScalars.java
@@ -1,8 +1,8 @@
 package graphql.scalars;
 
 import graphql.PublicApi;
-import graphql.scalars.datetime.DateTimeScalar;
 import graphql.scalars.datetime.DateScalar;
+import graphql.scalars.datetime.DateTimeScalar;
 import graphql.scalars.datetime.TimeScalar;
 import graphql.scalars.numeric.NegativeFloatScalar;
 import graphql.scalars.numeric.NegativeIntScalar;
@@ -17,12 +17,49 @@ import graphql.scalars.object.ObjectScalar;
 import graphql.scalars.url.UrlScalar;
 import graphql.schema.GraphQLScalarType;
 
+/**
+ * This is the API entry point for all the extended scalars
+ */
 @PublicApi
 public class ExtendedScalars {
 
-
+    /**
+     * An RFC-3339 compliant date time scalar that accepts string values like `1996-12-19T16:39:57-08:00` and produces
+     * `java.time.OffsetDateTime` objects at runtime.
+     * <p>
+     * Its {@link graphql.schema.Coercing#serialize(java.lang.Object)} and {@link graphql.schema.Coercing#parseValue(java.lang.Object)} methods
+     * accept OffsetDateTime, ZoneDateTime and formatted Strings as valid objects.
+     * <p>
+     * See the <a href="https://www.ietf.org/rfc/rfc3339.txt">rfc3339 spec</a> for more details on the format.
+     *
+     * @see java.time.OffsetDateTime
+     * @see java.time.ZonedDateTime
+     */
     public static GraphQLScalarType DateTime = new DateTimeScalar();
+
+    /**
+     * An RFC-3339 compliant date scalar that accepts string values like `1996-12-19` and produces
+     * `java.time.LocalDate` objects at runtime.
+     * <p>
+     * Its {@link graphql.schema.Coercing#serialize(java.lang.Object)} and {@link graphql.schema.Coercing#parseValue(java.lang.Object)} methods
+     * accept date {@link java.time.temporal.TemporalAccessor}s and formatted Strings as valid objects.
+     * <p>
+     * See the <a href="https://www.ietf.org/rfc/rfc3339.txt">rfc3339 spec</a> for more details on the format.
+     *
+     * @see java.time.LocalDate
+     */
     public static GraphQLScalarType Date = new DateScalar();
+    /**
+     * An RFC-3339 compliant time scalar that accepts string values like `6:39:57-08:00` and produces
+     * `java.time.OffsetTime` objects at runtime.
+     * <p>
+     * Its {@link graphql.schema.Coercing#serialize(java.lang.Object)} and {@link graphql.schema.Coercing#parseValue(java.lang.Object)} methods
+     * accept time {@link java.time.temporal.TemporalAccessor}s and formatted Strings as valid objects.
+     * <p>
+     * See the <a href="https://www.ietf.org/rfc/rfc3339.txt">rfc3339 spec</a> for more details on the format.
+     *
+     * @see java.time.OffsetTime
+     */
     public static GraphQLScalarType Time = new TimeScalar();
 
     /**
@@ -62,20 +99,63 @@ public class ExtendedScalars {
      * }
      * }
      * </pre>
+     *
+     * @see graphql.scalars.ExtendedScalars#Object
      */
     public static GraphQLScalarType Json = new JsonScalar();
 
+    /**
+     * A URL scalar that accepts URL strings and produces {@link java.net.URL} objects at runtime
+     */
     public static GraphQLScalarType Url = new UrlScalar();
 
+    /**
+     * An `Int` scalar that MUST be greater than zero
+     *
+     * @see graphql.Scalars#GraphQLInt
+     */
     public static GraphQLScalarType PositiveInt = new PositiveIntScalar();
+    /**
+     * An `Int` scalar that MUST be less than zero
+     *
+     * @see graphql.Scalars#GraphQLInt
+     */
     public static GraphQLScalarType NegativeInt = new NegativeIntScalar();
+    /**
+     * An `Int` scalar that MUST be less than or equal to zero
+     *
+     * @see graphql.Scalars#GraphQLInt
+     */
     public static GraphQLScalarType NonPositiveInt = new NonPositiveIntScalar();
+    /**
+     * An `Int` scalar that MUST be greater than or equal to zero
+     *
+     * @see graphql.Scalars#GraphQLInt
+     */
     public static GraphQLScalarType NonNegativeInt = new NonNegativeIntScalar();
 
+    /**
+     * An `Float` scalar that MUST be greater than zero
+     *
+     * @see graphql.Scalars#GraphQLFloat
+     */
     public static GraphQLScalarType PositiveFloat = new PositiveFloatScalar();
+    /**
+     * An `Float` scalar that MUST be less than zero
+     *
+     * @see graphql.Scalars#GraphQLFloat
+     */
     public static GraphQLScalarType NegativeFloat = new NegativeFloatScalar();
+    /**
+     * An `Float` scalar that MUST be less than or equal to zero
+     *
+     * @see graphql.Scalars#GraphQLFloat
+     */
     public static GraphQLScalarType NonPositiveFloat = new NonPositiveFloatScalar();
+    /**
+     * An `Float` scalar that MUST be greater than or equal to zero
+     *
+     * @see graphql.Scalars#GraphQLFloat
+     */
     public static GraphQLScalarType NonNegativeFloat = new NonNegativeFloatScalar();
-
-
 }
